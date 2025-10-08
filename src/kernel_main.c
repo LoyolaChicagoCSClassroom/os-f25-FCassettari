0954e01943e0fd88a57084f48aa0c822aa4fbcca
FCassettari <161382704+FCassettari@users.noreply.github.com>
2025-10-08 17:16:29 +0000

    interrupt

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,54 +1,75 @@
 
 #include <stdint.h>
 
-#define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
+#define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
 
-#define VGA_WIDTH   80
-#define VGA_HEIGHT  25
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
 #define VIDEO_MEMORY 0xB8000
 
-const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
+const unsigned int multiboot_header[] __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16 + MULTIBOOT2_HEADER_MAGIC), 0, 12};
 
-uint8_t inb (uint16_t _port) {
+uint8_t inb(uint16_t _port)
+{
     uint8_t rv;
-    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    __asm__ __volatile__("inb %1, %0" : "=a"(rv) : "dN"(_port));
     return rv;
 }
 
-int offset = 0; //where the next character goes
+int offset = 0; // where the next character goes
 
-void scroll() {
-    volatile uint16_t* vram = (uint16_t*)VIDEO_MEMORY;
-    for (int row = 1; row < VGA_HEIGHT; row++) {
-        for (int col = 0; col < VGA_WIDTH; col++) {
+void scroll()
+{
+    volatile uint16_t *vram = (uint16_t *)VIDEO_MEMORY;
+    for (int row = 1; row < VGA_HEIGHT; row++)
+    {
+        for (int col = 0; col < VGA_WIDTH; col++)
+        {
             vram[(row - 1) * VGA_WIDTH + col] = vram[row * VGA_WIDTH + col];
         }
     }
-    //clear bottom line
-    for (int col = 0; col < VGA_WIDTH; col++) {
+    // clear bottom line
+    for (int col = 0; col < VGA_WIDTH; col++)
+    {
         vram[(VGA_HEIGHT - 1) * VGA_WIDTH + col] = (' ' | (0x07 << 8));
     }
     offset -= VGA_WIDTH;
 }
 
-void putc(int data) {
-    volatile uint16_t* vram = (uint16_t*)VIDEO_MEMORY;
-    if (data == '\n') {
-        offset += VGA_WIDTH - (offset % VGA_WIDTH); //next line
-    } else {
+void putc(int data)
+{
+    volatile uint16_t *vram = (uint16_t *)VIDEO_MEMORY;
+    if (data == '\n')
+    {
+        offset += VGA_WIDTH - (offset % VGA_WIDTH); // next line
+    }
+    else
+    {
         vram[offset] = (data & 0xFF) | (0x07 << 8); // char + color
         offset++;
     }
-    if (offset >= VGA_WIDTH * VGA_HEIGHT) {
+    if (offset >= VGA_WIDTH * VGA_HEIGHT)
+    {
         scroll();
     }
 }
 
-void main() {
+void main()
+{
+    //remap_pic(); // Set upt the PC's programmable interrupt controller (PIC)
+    //load_gdt();  // Load the global descriptor table, part of the vector table
+    //init_idt();  // initialize the interrupt descriptor table
+    asm("sti");
 
-    for (int i = 7; i > 0; i--) { esp_printf(putc, "Hello\n"); }
+    for (int i = 7; i > 0; i--)
+    {
+        esp_printf(putc, "Hello\n");
+    }
 
-    for (int i = 1008; i > 0; i--) { putc('j'); }
+    for (int i = 1008; i > 0; i--)
+    {
+        putc('j');
+    }
 
     uint32_t esp_val;
     __asm__ __volatile__("mov %%esp, %0" : "=r"(esp_val));
